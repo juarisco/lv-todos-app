33b9e743d1ce0ec68bd51a9853c78962f92bfb96
juarisco <juarisco@outlook.com>
2019-04-19 17:41:39 -0500

    10. Model view controller

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
 class TodosController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        return view('todos.index')->with('todos', Todo::all());
     }
 }
